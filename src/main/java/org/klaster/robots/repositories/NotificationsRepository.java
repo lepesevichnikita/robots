dd5ee626ad51e264ffe3ab2b6e09c0aba087b061
Nikita Lepesevich <lepesevich.nikita@yandex.ru>
2019-10-18 12:58:11 +0300

    feat:
    NotificationRepository was added to provide access to notification
    
    Signed-off-by: Nikita Lepesevich <lepesevich.nikita@yandex.ru>

---
+++ src/main/java/org/klaster/robots/repositories/NotificationsRepository.java
@@ -0,0 +1,13 @@
+package org.klaster.robots.repositories;
+
+import org.klaster.robots.models.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Nikita Lepesevich <lepesevich.nikita@yandex.ru> on 10/17/19
+ * @project robots
+ */
+@Component("notificationsRepository")
+public interface NotificationsRepository extends JpaRepository<Notification, Long> {
+}
