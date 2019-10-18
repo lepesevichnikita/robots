6bb570a9f53a127d626206c0092a5947ab7c89d7
Nikita Lepesevich <lepesevich.nikita@yandex.ru>
2019-10-18 13:51:00 +0300

    feat:
    RobotsApplication was inherited from SpringBootServletInitializer to work with JSP
    Override configure method to extend SpringBootServletInitializer
    
    Signed-off-by: Nikita Lepesevich <lepesevich.nikita@yandex.ru>

--- src/main/java/org/klaster/robots/RobotsApplication.java
+++ src/main/java/org/klaster/robots/RobotsApplication.java
@@ -2,9 +2,16 @@ package org.klaster.robots;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RobotsApplication {
+public class RobotsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RobotsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RobotsApplication.class, args);
