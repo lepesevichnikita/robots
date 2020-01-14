108ff4159dbb096e524da713679b525f937e2b02
Nikita Lepesevich <nikita.lepesevich.jazzteam@gmail.com>
2020-01-14 11:24:36 +0300

    chore:
    1. Move "robots" in submodule
    
    Signed-off-by: Nikita Lepesevich <nikita.lepesevich.jazzteam@gmail.com>

---
+++ alg/src/test/java/org/klaster/AppTest.java
@@ -0,0 +1,20 @@
+package org.klaster;
+
+import static org.testng.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
