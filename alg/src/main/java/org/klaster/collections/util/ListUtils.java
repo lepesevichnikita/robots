de5281f4eac63e4497f67432498764dc35d59785
Nikita Lepesevich <nikita.lepesevich.jazzteam@gmail.com>
2020-01-17 09:21:04 +0300

    style(collections):
    1. Utils were reformated

--- alg/src/main/java/org/klaster/collections/util/ListUtils.java
+++ alg/src/main/java/org/klaster/collections/util/ListUtils.java
@@ -38,17 +38,15 @@ public class ListUtils {
   }
 
   private static void appendItemsToList(List<Long> list, Long itemsCount) {
-    LongStream.range(0, itemsCount).forEach((long currentItemNumber) -> {
-      list.add(HelperUtils.getRandomNumber(HelperUtils.getMaxValueShiftedBySize(itemsCount),
-                                           HelperUtils.getMinValueShiftedBySize(itemsCount)));
-    });
+    LongStream.range(0, itemsCount).forEach((long currentItemNumber) -> list
+        .add(HelperUtils.getRandomNumber(HelperUtils.getMaxValueShiftedBySize(itemsCount),
+            HelperUtils.getMinValueShiftedBySize(itemsCount))));
   }
 
   private static void insertItemsIntoHeadOfList(List<Long> list, Long itemsCount) {
-    LongStream.range(0, itemsCount).forEach((long currentItemNumber) -> {
-      list.add(0, HelperUtils.getRandomNumber(HelperUtils.getMaxValueShiftedBySize(itemsCount),
-                                              HelperUtils.getMinValueShiftedBySize(itemsCount)));
-    });
+    LongStream.range(0, itemsCount).forEach((long currentItemNumber) -> list
+        .add(0, HelperUtils.getRandomNumber(HelperUtils.getMaxValueShiftedBySize(itemsCount),
+            HelperUtils.getMinValueShiftedBySize(itemsCount))));
   }
 
   public static Long measureAccessToItemFromMiddleOfList(List<Long> list,
@@ -56,22 +54,17 @@ public class ListUtils {
                                                          ParametrizedCallback<Long> callback) {
     appendItemsToList(list, itemsCount);
     return HelperUtils.measureMethod(
-        () -> {
-          LongStream.range(itemsCount / 2, itemsCount).forEach((long currentItemIndex) -> {
-            callback.execute(list.get(Math.toIntExact(currentItemIndex)));
-          });
-        });
+        () -> LongStream.range(itemsCount / 2, itemsCount).forEach(
+            (long currentItemIndex) -> callback
+                .execute(list.get(Math.toIntExact(currentItemIndex)))));
   }
 
   public static Long measureAssigningByIndexFromMiddle(List<Long> list, Long itemsCount) {
     appendItemsToList(list, itemsCount);
     return HelperUtils.measureMethod(
-        () -> {
-          LongStream.range(itemsCount / 2, itemsCount).forEach((long currentItemIndex) -> {
-            list.add(Math.toIntExact(currentItemIndex),
-                     HelperUtils.getRandomNumber(HelperUtils.getMaxValueShiftedBySize(itemsCount),
-                                                 HelperUtils.getMinValueShiftedBySize(itemsCount)));
-          });
-        });
+        () -> LongStream.range(itemsCount / 2, itemsCount)
+            .forEach((long currentItemIndex) -> list.add(Math.toIntExact(currentItemIndex),
+                HelperUtils.getRandomNumber(HelperUtils.getMaxValueShiftedBySize(itemsCount),
+                    HelperUtils.getMinValueShiftedBySize(itemsCount)))));
   }
 }
