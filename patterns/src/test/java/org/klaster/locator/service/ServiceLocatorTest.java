774dc57e769073ea2b8cab71e8f3fcba93d76ef4
Nikita Lepesevich <nikita.lepesevich.jazzteam@gmail.com>
2020-02-04 06:41:26 +0300

    test (patterns):
    1. String literals were moved to constants if possible
    
    Signed-off-by: Nikita Lepesevich <lepesevich.nikita@yandex.ru>

--- patterns/src/test/java/org/klaster/locator/service/ServiceLocatorTest.java
+++ patterns/src/test/java/org/klaster/locator/service/ServiceLocatorTest.java
@@ -21,6 +21,9 @@ import org.testng.annotations.Test;
 
 public class ServiceLocatorTest {
 
+  private static final String FIRST_SERVICE = "org.klaster.locator.service.FirstService";
+  private static final String SECOND_SERVICE = "org.klaster.locator.service.SecondService";
+  private static final String UNDEFINED_SERVICE = "UndefinedService";
   private ServiceLocator serviceLocator;
 
   @BeforeMethod
@@ -30,20 +33,20 @@ public class ServiceLocatorTest {
 
   @Test
   public void findsCorrectServiceIfItExists() {
-    Service firstService = serviceLocator.lookup("org.klaster.locator.service.FirstService");
+    Service firstService = serviceLocator.lookup(FIRST_SERVICE);
     assertThat(firstService, isA(FirstService.class));
   }
 
   @Test
   public void findsServicesCorrectly() {
-    Service firstService = serviceLocator.lookup("org.klaster.locator.service.FirstService");
-    Service secondService = serviceLocator.lookup("org.klaster.locator.service.SecondService");
+    Service firstService = serviceLocator.lookup(FIRST_SERVICE);
+    Service secondService = serviceLocator.lookup(SECOND_SERVICE);
     assertThat(firstService, not(isA(secondService.getClass())));
   }
 
   @Test
   public void firstServiceActsAsExpected() {
-    Service firstService = serviceLocator.lookup("org.klaster.locator.service.FirstService");
+    Service firstService = serviceLocator.lookup(FIRST_SERVICE);
     String expectedResult = "First service completed";
     assertThat(firstService.execute(), equalTo(expectedResult));
   }
@@ -51,21 +54,21 @@ public class ServiceLocatorTest {
 
   @Test
   public void secondServiceActsAsExpected() {
-    Service secondService = serviceLocator.lookup("org.klaster.locator.service.SecondService");
+    Service secondService = serviceLocator.lookup(SECOND_SERVICE);
     String expectedResult = "Second service completed";
     assertThat(secondService.execute(), equalTo(expectedResult));
   }
 
   @Test
   public void returnsNullIfServiceWithPassedNameDoesntExist() {
-    Service undefinedService = serviceLocator.lookup("UndefinedService");
+    Service undefinedService = serviceLocator.lookup(UNDEFINED_SERVICE);
     assertThat(undefinedService, nullValue());
   }
 
   @Test
   public void findsSameInstanceOfServiceEveryTime() {
-    Service firstTimeFoundService = serviceLocator.lookup("org.klaster.locator.service.FirstService");
-    Service secondTimeFoundService = serviceLocator.lookup("org.klaster.locator.service.FirstService");
+    Service firstTimeFoundService = serviceLocator.lookup(FIRST_SERVICE);
+    Service secondTimeFoundService = serviceLocator.lookup(FIRST_SERVICE);
     assertThat(firstTimeFoundService, equalTo(secondTimeFoundService));
   }
 }
