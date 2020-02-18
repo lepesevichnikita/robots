974c5008b71eb2c7841195ce8ef472abdde60e1a
Nikita Lepesevich <nikita.lepesevich.jazzteam@gmail.com>
2020-02-18 12:18:55 +0300

    test (alg)
    1. DataProvider was moved from RectangelGeneratorTest to testng.xml
    
    Signed-off-by: Nikita Lepesevich <nikita.lepesevich.jazzteam@gmail.com>

--- alg/src/test/java/org/klaster/tasks/files/service/RectangleGeneratorTest.java
+++ alg/src/test/java/org/klaster/tasks/files/service/RectangleGeneratorTest.java
@@ -15,7 +15,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.nullValue;
 
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class RectangleGeneratorTest {
@@ -23,23 +23,13 @@ public class RectangleGeneratorTest {
   private final RectangleGenerator rectangleGenerator = new RectangleGenerator();
   private final WordsGrouper wordsGrouper = new WordsGrouper();
 
-  @DataProvider
-  public static Object[][] wordsAndRectangles() {
-    return new Object[][]{
-        {new String[]{"agent", "house", "notch", "ahn", "goo", "eut", "nsc", "teh"},
-            new String[]{"agent", "house", "notch"}},
-        {new String[]{"a"}, new String[]{"a"}},
-        {new String[]{"aa"}, new String[]{"aa", "aa"}},
-        {new String[]{"123", "321", "478", "134", "227", "318"}, new String[]{"123", "321", "478"}},
-    };
-  }
-
-  @Test(dataProvider = "wordsAndRectangles")
-  public void createsRectangles(String[] dictionary, String[] expectedRows) {
-    wordsGrouper.setDictionary(dictionary);
+  @Test
+  @Parameters({"dictionary", "expectedRows"})
+  public void createsRectangles(String dictionary, String expectedRows) {
+    wordsGrouper.setDictionary(dictionary.split(" "));
     rectangleGenerator.setWordsContainers(wordsGrouper.groupWordsByLength());
     assertThat(rectangleGenerator.generateFirstMaximumPossibleRectangle()
-                                 .getRows(), contains(expectedRows));
+                                 .getRows(), contains(expectedRows.split(" ")));
   }
 
   @Test
