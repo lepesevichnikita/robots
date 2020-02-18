4217e15a944bab28d4523c26ec45d4c135d76c5d
Nikita Lepesevich <nikita.lepesevich.jazzteam@gmail.com>
2020-02-18 11:59:19 +0300

    test (alg)
    1. DataProvider was removed from WordsContainerTest to testng.xml
    
    Signed-off-by: Nikita Lepesevich <nikita.lepesevich.jazzteam@gmail.com>

--- alg/src/test/java/org/klaster/tasks/files/model/WordsContainerTest.java
+++ alg/src/test/java/org/klaster/tasks/files/model/WordsContainerTest.java
@@ -13,38 +13,26 @@ package org.klaster.tasks.files.model;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.testng.annotations.DataProvider;
+import java.util.Arrays;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class WordsContainerTest {
 
-  @DataProvider
-  public static Object[][] wordsData() {
-    return new Object[][]{
-        {1, new String[]{""}, new String[]{}},
-        {1, new String[]{"first", "cat", "an"}, new String[]{}},
-        {0, new String[]{""}, new String[]{""}},
-        {0, new String[]{}, new String[]{}},
-        {2, new String[]{"first", "cat", "an"}, new String[]{"an"}},
-        {
-            3,
-            new String[]{null, "cat", "tom", "and", "mouse", "jerry"},
-            new String[]{"cat", "tom", "and"}
-        },
-        {4, new String[]{null, "cat", "tom", "and", "mouse", "jerry"}, new String[]{}},
-        {5, new String[]{"cat", "tom", "and", "mouse", "jerry"}, new String[]{"mouse", "jerry"}},
-        {0, new String[]{null, null, null, null, null}, new String[]{}}
-    };
-  }
-
-  @Test(dataProvider = "wordsData")
+  @Test
+  @Parameters({"wordsLength", "sourceWords", "addedWords"})
   public void addsItemsOnlyOfSameLength(
-      int wordsLength, String[] sourceWords, String[] addedWords) {
+      int wordsLength, String sourceWords, String addedWords) {
     WordsContainer wordsContainer = new WordsContainer(wordsLength);
-    for (String sourceWord : sourceWords) {
-      wordsContainer.addWord(sourceWord);
-    }
+    Arrays.stream(sourceWords.split(" "))
+          .map(word -> word.equals("null")
+                       ? null
+                       : word)
+          .forEach(word -> {
+            wordsContainer.addWord(word);
+          });
+    String[] expectedWords = addedWords.split(" ");
     assertThat(wordsContainer.getWords()
-                             .toArray(), equalTo(addedWords));
+                             .toArray(), equalTo(expectedWords));
   }
 }
